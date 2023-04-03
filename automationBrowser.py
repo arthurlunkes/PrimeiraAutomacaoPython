5d3c46e183633f44cb3fec81cd736ca057547c32
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-04-03 10:21:53 -0300

    Initial commit

---
+++ automationBrowser.py
@@ -0,0 +1,17 @@
+import pyautogui
+
+#clica no ícone do navegador
+pyautogui.click(172,746,duration=1.5)
+
+#clica na primeira página aberta
+pyautogui.click(153,15,duration=1.5)
+
+#clica na barra de url
+pyautogui.click(329,49,duration=1.5)
+
+#faz a pesquisa do site desejado
+pyautogui.write('google.com')
+pyautogui.press('enter')
+
+#nesse comando estava apenas testando.
+#pyautogui.confirm(text='Sua senha está expirada', title='Teste', buttons=['OK', 'Cancel'])
