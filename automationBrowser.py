a43cfa725c789c2363edc1bce2d4221e7f77337d
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-04-22 19:25:04 -0300

    Update automationBrowser.py

--- automationBrowser.py
+++ automationBrowser.py
@@ -1,13 +1,13 @@
 import pyautogui
 
 #clica no ícone do navegador
-pyautogui.click(172,746,duration=1.5)
+pyautogui.click(35,196,duration=1.5)
 
 #clica na primeira página aberta
-pyautogui.click(153,15,duration=1.5)
+pyautogui.click(180,44,duration=1.5)
 
 #clica na barra de url
-pyautogui.click(329,49,duration=1.5)
+pyautogui.click(436,94,duration=1.5)
 
 #faz a pesquisa do site desejado
 pyautogui.write('youtube.com')
